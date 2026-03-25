66b7bb7f4a5d8f32bc49ee68ffca41837ca7b0f3
Simon Kertonegoro <payments@parallelplanes.io>
2026-03-26 07:39:36 +1100

    Add CC param to email send/reply, is_automated filter to threads, involved_user_ids to SMS

--- src/data/endpoints/sms.ts
+++ src/data/endpoints/sms.ts
@@ -9,8 +9,8 @@ export const SMS: ResourceGroup = {
   label: 'SMS',
   description: 'Send and receive SMS messages. View conversations and message history.',
   endpoints: [
-    { method: 'GET', path: '/sms/conversations', description: 'List SMS conversations.', scopes: ['sms:read'], isWrite: false },
-    { method: 'GET', path: '/sms/conversations/:id', description: 'Retrieve an SMS conversation.', scopes: ['sms:read'], isWrite: false, params: [{ name: 'id', type: 'uuid', required: true, description: 'Conversation ID', in: 'path' }] },
+    { method: 'GET', path: '/sms/conversations', description: 'List SMS conversations. Returns involved_user_ids showing which team members have interacted.', scopes: ['sms:read'], isWrite: false },
+    { method: 'GET', path: '/sms/conversations/:id', description: 'Retrieve an SMS conversation with involved_user_ids and linked entities.', scopes: ['sms:read'], isWrite: false, params: [{ name: 'id', type: 'uuid', required: true, description: 'Conversation ID', in: 'path' }] },
     { method: 'GET', path: '/sms/conversations/:id/messages', description: 'List messages in a conversation.', scopes: ['sms:read'], isWrite: false, params: [{ name: 'id', type: 'uuid', required: true, description: 'Conversation ID', in: 'path' }] },
     { method: 'POST', path: '/sms/send', description: 'Send an SMS message.', scopes: ['sms:send'], isWrite: true, params: [{ name: 'to_number', type: 'string', required: true, description: 'Recipient phone number (E.164)', in: 'body' }, { name: 'message', type: 'string', required: true, description: 'SMS message body', in: 'body' }, { name: 'phone_number_id', type: 'uuid', required: false, description: 'Sender phone number ID', in: 'body' }] },
   ],
